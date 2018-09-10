75b59f9352475e2966f1045f3b15621d034768e2
inexxt <jac.karwowski@gmail.com>
2018-09-10 15:21:50 +0200

    Update

---
+++ examples/iteration7_sklearn.py
@@ -0,0 +1,42 @@
+import time
+from typing import Tuple
+
+import golem_remote.golem_remote as golem
+from golem_remote import GolemClient
+from golem_remote.runf_helpers import SubtaskID
+
+##############
+# golem init #
+##############
+
+golem.init(
+    class_=GolemClient,
+    timeout=300
+)
+
+#######################
+# function definition #
+#######################
+
+
+
+secret_sauce = "Secret"
+
+@golem.remote
+def func(arg1: int, arg2: int, kwarg1: str="abc", kwarg2: str="def") \
+        -> Tuple[int, str, str]:
+    time.sleep(1)  # expensive call
+    print(f"Running func: {arg1} {arg2} {kwarg1} {kwarg2}")
+    return (arg1 + arg2, kwarg1 + kwarg2, secret_sauce)
+
+##################
+# function calls #
+##################
+
+res_id1: SubtaskID = func.remote(1, 2, kwarg1="abcd")
+
+# SubtaskIDs work as in ray
+res1 = golem.get(res_id1)
+print(f"Result: {res1}")
+assert res1 == (1 + 2, "abcd" + "def", secret_sauce)
+print("Assert: True")
--- run_test.sh
+++ run_test.sh
@@ -2,10 +2,10 @@
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-pkill redis-server
+pkill redis-server > /dev/null 2>&1
 
-redis-server &
-redis-cli flushall
+redis-server > /dev/null 2>&1 &
+redis-cli flushall > /dev/null 2>&1
 
 set -e
 
@@ -14,17 +14,17 @@ PYTHON_PATH=/home/jacek/golemenv/bin/python3
 
 cd ~/golem_orig/
 
-$PYTHON_PATH golemapp.py --log-level INFO --datadir ~/golem_data/data8_r --accept-terms --protocol_id 97 --password "k123@" -p localhost:40103 -r localhost:61000 &
+$PYTHON_PATH golemapp.py --log-level INFO --datadir ~/golem_data/data8_r --accept-terms --protocol_id 97 --password "k123@" -p localhost:40103 -r localhost:61000 > /dev/null 2>&1 &
 
-sleep 2
+sleep 3
 
-$PYTHON_PATH golemapp.py --log-level INFO --datadir ~/golem_data/data10_p --accept-terms --protocol_id 97 --password "k123@" -p localhost:40102 -r localhost:61001 &
+$PYTHON_PATH golemapp.py --log-level INFO --datadir ~/golem_data/data10_p --accept-terms --protocol_id 97 --password "k123@" -p localhost:40102 -r localhost:61001 > /dev/null 2>&1 &
 
-sleep 2
+sleep 3
 
 cd ~/golem_remote/examples
 
-sleep 10
+sleep 12
 
 PYTHON_PATH=/home/jacek/anaconda3/bin/python3
 $PYTHON_PATH iteration6_golem_remote_package.py
