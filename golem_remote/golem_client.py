d1e9ea71ee4d6b763d37c562a2da14a7705dc1c2
inexxt <jac.karwowski@gmail.com>
2018-09-19 23:41:16 +0200

    Update

--- golem_remote/golem_client.py
+++ golem_remote/golem_client.py
@@ -166,10 +166,10 @@ class GolemClient(GolemClientInterface):
         with tempfile.TemporaryDirectory() as tmp:
             task_definition_path = Path(tmp, "definition.json")
 
-            dest_description, dest_to_local = initialize_task_files(tmp, self.task_files)
+            dest_to_local = initialize_task_files(tmp, self.task_files)
             fill_task_definition(self.task_definition_template_path, self.queue_host,
                                  self.queue_port, task_definition_path, self.number_of_subtasks,
-                                 set(dest_to_local.values()) | {dest_description})
+                                 set(dest_to_local.values()))
             logger.info(f"Task definition saved in {task_definition_path}")
             stdout = _run_cmd(self._build_start_task_cmd(task_definition_path))
 
