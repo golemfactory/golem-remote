c426982f58c1222a141e465646000fed2811382a
inexxt <jac.karwowski@gmail.com>
2018-09-21 03:16:12 +0200

    Update

--- integration_test.sh
+++ integration_test.sh
@@ -2,7 +2,8 @@
 
 trap "trap - SIGTERM && kill -- -$$ && rm -rf ./examples/abcd" SIGINT SIGTERM EXIT
 
-
+DATADIR_R=/home/jacek/golem_data/data18_r
+DATADIR_P=/home/jacek/golem_data/data12_p
 
 pkill redis-server > /dev/null
 
@@ -14,11 +15,11 @@ set -e
 
 cd ~/golem_orig/
 
-/home/jacek/golemenv/bin/python3 golemapp.py --log-level INFO --datadir ~/golem_data/data18_r --accept-terms --protocol_id 97 --password "k123@" -p localhost:40103 -r localhost:61000 > /dev/null &
+/home/jacek/golemenv/bin/python3 golemapp.py --log-level INFO --datadir $DATADIR_R --accept-terms --protocol_id 97 --password "k123@" -p localhost:40103 -r localhost:61000 > /dev/null &
 
 sleep 3
 
-/home/jacek/golemenv/bin/python3 golemapp.py --log-level INFO --datadir ~/golem_data/data12_p --accept-terms --protocol_id 97 --password "k123@" -p localhost:40102 -r localhost:61001 > /dev/null &
+/home/jacek/golemenv/bin/python3 golemapp.py --log-level INFO --datadir $DATADIR_P --accept-terms --protocol_id 97 --password "k123@" -p localhost:40102 -r localhost:61001 > /dev/null &
 
 sleep 3
 
@@ -26,8 +27,8 @@ cd ~/golem_remote/examples
 
 sleep 18
 
+
 PYTHON_PATH=/home/jacek/anaconda3/bin/python3
-#$PYTHON_PATH iteration6_golem_remote_package.py
 $PYTHON_PATH iteration7_task_files.py
 
 echo "Done"
